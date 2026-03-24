ef952c01b69b8cdcfb98c98801edbb66aaf4fb0e
innovation-sys <innovation@ekot.nz>
2026-03-24 14:07:10 +1300

    Create tailwind.config.js with content paths

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,ts,html}",
+    "./components/**/*.{js,ts,jsx,ts,html}",
+    "./app/**/*.{js,ts,jsx,ts,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
